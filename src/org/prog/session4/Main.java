81c4d4e3fb17e29d702bca0f92d18b14349efd2e
YuliiaNo <spenta.armait@gmail.com>
2026-04-07 11:24:26 +0300

    homework-4 Nosenko

--- src/org/prog/session4/Main.java
+++ src/org/prog/session4/Main.java
@@ -4,48 +4,76 @@ package org.prog.session4;
 //TODO: Write class PhoneShop where you can buy phone that is not black and iphone
 //TODO: PhoneShop must return phone with requested brand and color
 
-public class Main {
-
-    /**
-     * #Car.brand#######################
-     * #################################
-     * #################################
-     * ########(Car.color)############## <---- car1
-     * ########(Car.color)############## <---- car2
-     * #################################
-     * #################################
-     * #################################
-     * =================================
-     * #########10######################
-     */
-
-    public static void main(String[] args) {
-        PoliceStation.checkIfCarHasFines("asdasd");
 
 
-        String s1 = "s1";
-        String s2 = "s1";
+class Phone {
+    String brand;
+    String color;
 
-        System.out.println(s1.equals(s2));
-//        String s3;
-
-//        Car aliceCar = null;
-//        System.out.println(aliceCar.color);
-
-        CarService service = new CarService();
-
-        Car.brand = "Volvo";
-
-        Car car = new Car("AA0110AA");
-        service.paintCar("Red", car);
-        car.goTo("Lviv");
-
-        Car car2 = new Car("AA0120AA");
-        service.paintCar("Blue", car2);
-        car2.goTo("Odessa");
+    public Phone(String brand, String color) {
+        this.brand = brand;
+        this.color = color;
     }
 
-    public static void paintCar(Car car, String color) {
-        car.color = color;
+    public void ring() {
+        System.out.println("Телефон " + brand + " " + color + " кольору робить: дінь-дінь");
+    }
+}
+
+class PhoneShop {
+
+
+    public static boolean isPhoneAvailable(String brand, String color) {
+        boolean found = true;
+
+        if (brand.equals("iPhone") || color.equals("black")) {
+            found = false;
+        }
+
+        return found;
+    }
+
+    public Phone buyPhone(String brand, String color) {
+
+        if (isPhoneAvailable(brand, color)) {
+            System.out.println("Телефон " + brand + " " + color + " є в магазині");
+
+            Phone phone = new Phone(brand, color);
+            return phone;
+        }
+
+        System.out.println("Такого телефону немає в магазині");
+        return null;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s1 = "Samsung";
+        String s2 = "Samsung";
+        System.out.println(s1.equals(s2));
+
+        PhoneShop shop = new PhoneShop();
+
+        Phone phone1 = shop.buyPhone("Samsung", "blue");
+        if (phone1 != null) {
+            phone1.ring();
+        }
+
+        System.out.println("-----------");
+
+        Phone phone2 = shop.buyPhone("iPhone", "white");
+        if (phone2 != null) {
+            phone2.ring();
+        }
+
+        System.out.println("-----------");
+
+        Phone phone3 = shop.buyPhone("Nokia", "black");
+        if (phone3 != null) {
+            phone3.ring();
+        }
     }
 }
